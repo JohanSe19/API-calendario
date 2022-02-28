7ab532be52dd419f303c9b9e4b9b8838fff72a71
Sebastian Carvajal <johan.carvajal@carvajal.com>
2022-02-28 09:16:42 -0500

    comment routes/api app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
 
-app.use("/api", require(path.join(__dirname, "routes/api")));
+/* app.use("/api", require(path.join(__dirname, "routes/api"))); */
 // Error Handler
 app.use(function (err, req, res, next) {
   res.status(500);
